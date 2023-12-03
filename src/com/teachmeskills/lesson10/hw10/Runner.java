28cbb4b768bccb596114696e86fb0a16e1c7fb29
Savva <sav.van.ivan3@mail.ru>
2023-12-03 15:29:55 +0300

    HW10

--- src/com/teachmeskills/lesson10/hw10/Runner.java
+++ src/com/teachmeskills/lesson10/hw10/Runner.java
@@ -6,6 +6,16 @@ import com.teachmeskills.lesson10.hw10.model.card.impl.MasterCard;
 import com.teachmeskills.lesson10.hw10.model.card.impl.VisaCard;
 import com.teachmeskills.lesson10.hw10.model.client.Client;
 
+/**
+ * There is a client.
+ * The client can have any number of cards.
+ * The card has three fields - cvv code, card number and amount on the card.
+ * Display a message indicating how many identical cards the client has.
+ * The field describing the amount on the card should not be included in the comparison.
+ * Those. two multiples with the same cvv codes and numbers, but different amounts are considered the same.
+ * Provide different options for the output message - for example,
+ * the client does not have any cards, the client does not have identical cards, and other possible situations.
+ */
 public class Runner {
     public static void main(String[] args) {
         int count = 0;
